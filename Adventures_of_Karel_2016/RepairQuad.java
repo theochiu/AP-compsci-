ae1e48c3557922d89c91547999f6e2349ecb17ab
Theodore Chiu <tchiu17@shs.shschools.org>
2016-08-25 08:33:54 -0700

    edits

--- Adventures_of_Karel_2016/RepairQuad.java
+++ Adventures_of_Karel_2016/RepairQuad.java
@@ -1,26 +1,40 @@
+// Copyright Theodore Chiu 2016, All rights reserved 
 
 import stanford.karel.*;
 
 public class RepairQuad extends Karel
 {
 
-    public void run()
-    {
+	public void run()
+	{
+		while (frontisClear()){
+			repairColumn();
+			moveNext();
+		}
+	}
 
-    }
+	public void moveNext(){
+		for (int i=0;i<4;i++){
+			move();
+		}
+	}
 
-    /* IGNORE THE CODE BELOW.  (Don't delete it, but we
-     * had to include it to get Karel to play nicely with
-     * BlueJ.  You don't need to understand what it means
-     * at all.  */
-    public static void launchKarelEnvironment()
-    {
-        String[] args = new String[1];
-        String name = new SecurityManager() {
-            public String className() 
-                {return this.getClassContext()[1].getCanonicalName();}
-            }.className();
-        args[0] = "code=" + name;         
-        Karel.main(args);
-    }
+	public void repairColumn(){
+		
+	}
+
+	/* IGNORE THE CODE BELOW.  (Don't delete it, but we
+	 * had to include it to get Karel to play nicely with
+	 * BlueJ.  You don't need to understand what it means
+	 * at all.  */
+	public static void launchKarelEnvironment()
+	{
+		String[] args = new String[1];
+		String name = new SecurityManager() {
+			public String className() 
+				{return this.getClassContext()[1].getCanonicalName();}
+			}.className();
+		args[0] = "code=" + name;         
+		Karel.main(args);
+	}
 }
