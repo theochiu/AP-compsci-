cd988169aba9d4b4282adc790e767af8f6a7b2d3
Theodore Chiu <tchiu17@shs.shschools.org>
2016-08-26 11:47:32 -0700

    ww

--- Adventures_of_Karel_2016/CleanStairs.java
+++ Adventures_of_Karel_2016/CleanStairs.java
@@ -4,23 +4,32 @@ import stanford.karel.*;
 public class CleanStairs extends Karel
 {
 
-    public void run()
-    {
+	public void run()
+	{	// finds stairs
+		while (frontIsClear()){
+			move();
+		}
 
-    }
+	}
 
-    /* IGNORE THE CODE BELOW.  (Don't delete it, but we
-     * had to include it to get Karel to play nicely with
-     * BlueJ.  You don't need to understand what it means
-     * at all.  */
-    public static void launchKarelEnvironment()
-    {
-        String[] args = new String[1];
-        String name = new SecurityManager() {
-            public String className() 
-                {return this.getClassContext()[1].getCanonicalName();}
-            }.className();
-        args[0] = "code=" + name;         
-        Karel.main(args);
-    }
+	public void turnRight(){
+		for(int i=0;i<3;i++){
+			turnLeft();
+		}
+	}
+
+	/* IGNORE THE CODE BELOW.  (Don't delete it, but we
+	 * had to include it to get Karel to play nicely with
+	 * BlueJ.  You don't need to understand what it means
+	 * at all.  */
+	public static void launchKarelEnvironment()
+	{
+		String[] args = new String[1];
+		String name = new SecurityManager() {
+			public String className() 
+				{return this.getClassContext()[1].getCanonicalName();}
+			}.className();
+		args[0] = "code=" + name;         
+		Karel.main(args);
+	}
 }
