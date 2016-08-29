afc8786141abcb89a8c763258c0342e84aa3d369
Theodore Chiu <tchiu17@shs.shschools.org>
2016-08-29 14:17:13 -0700

    ...

--- Adventures_of_Karel_2016/MidpointFinder.java
+++ Adventures_of_Karel_2016/MidpointFinder.java
@@ -1,26 +1,57 @@
-
+// Theodore Chiu
 import stanford.karel.*;
 
 public class MidpointFinder extends Karel
 {
 
-    public void run()
-    {
+	public void layoutRows(){
+		while (!frontIsBlocked()){
+			move();
+			putBeeper();
+		} // remove last beeper
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+	}
 
-    }
+	public void run()
+	{
+		layoutRows();
+		while (beepersPresent()){
+			bounceAround();
+		}
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		turnLeft();
+	}
 
-    /* IGNORE THE CODE BELOW.  (Don't delete it, but we
-     * had to include it to get Karel to play nicely with
-     * BlueJ.  You don't need to understand what it means
-     * at all.  */
-    public static void launchKarelEnvironment()
-    {
-        String[] args = new String[1];
-        String name = new SecurityManager() {
-            public String className() 
-                {return this.getClassContext()[1].getCanonicalName();}
-            }.className();
-        args[0] = "code=" + name;         
-        Karel.main(args);
-    }
+	public void bounceAround(){
+		while (beepersPresent()){
+			move();
+		}
+		turnLeft();
+		turnLeft();
+		move();
+		pickBeeper();
+		move();
+
+	}
+
+	/* IGNORE THE CODE BELOW.  (Don't delete it, but we
+	 * had to include it to get Karel to play nicely with
+	 * BlueJ.  You don't need to understand what it means
+	 * at all.  */
+	public static void launchKarelEnvironment()
+	{
+		String[] args = new String[1];
+		String name = new SecurityManager() {
+			public String className() 
+				{return this.getClassContext()[1].getCanonicalName();}
+			}.className();
+		args[0] = "code=" + name;         
+		Karel.main(args);
+	}
 }
