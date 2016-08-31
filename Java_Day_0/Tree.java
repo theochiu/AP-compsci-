3ce8afbfbd9f8aa24905f78d201fce632e1a6e39
Theodore Chiu <tchiu17@shs.shschools.org>
2016-08-31 11:36:05 -0700

    ...

--- Java_Day_0/Tree.java
+++ Java_Day_0/Tree.java
@@ -7,10 +7,23 @@ import java.awt.event.*;
 public class Tree extends GraphicsProgram
 {
 
-    public void run()
-    {
+	public void run()
+	{
+		GRect trunk = new GRect(50,50,20,100);
+		trunk.setColor(Color.yellow);
+		trunk.setFilled(true);
+		add(trunk);
 
-    }
+		GOval leaves = new GOval(40,15,40,40);
+		leaves.setColor(Color.green);
+		leaves.setFilled(true);
+		add(leaves);
+
+		GLabel text = new GLabel("This is a tree", 20 , 180);
+		text.setColor(Color.red);
+		text.setFont("Palatino-Bold-24");
+		add(text);
+	}
 
 
 }
