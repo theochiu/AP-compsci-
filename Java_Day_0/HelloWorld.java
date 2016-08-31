2f605aedfcf148d73a8c1e28c2948da687503650
Theodore Chiu <tchiu17@shs.shschools.org>
2016-08-31 11:06:12 -0700

    added files needed for class

---
+++ Java_Day_0/HelloWorld.java
@@ -0,0 +1,14 @@
+
+import acm.program.*;
+
+public class HelloWorld extends ConsoleProgram
+{
+
+    public void run()
+    {
+        println("Hello world");
+        println("Today was a late start Wednesday!");
+    }
+
+
+}
---
+++ Java_Day_0/helloworld2.java
@@ -0,0 +1,5 @@
+public class helloworld2{
+    public static void main(){
+        System.out.println("Hello World");
+    }
+}
