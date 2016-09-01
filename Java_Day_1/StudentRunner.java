f17fc9511c0ab90ef39c0a9bef3c039e826d57f3
Theodore Chiu <tchiu17@shs.shschools.org>
2016-09-01 13:53:54 -0700

    ...

---
+++ Java_Day_1/StudentRunner.java
@@ -0,0 +1,14 @@
+
+import acm.program.*;
+
+public class StudentRunner extends ConsoleProgram
+{
+
+    public void run()
+    {
+        Student stud = new Student("Theo");
+        stud.takeQuiz(8);
+    }
+
+
+}
