80b3b48db9a7c053316905e678491ed20b0226a9
theochiu <theochiu.me@gmail.com>
2016-08-31 20:06:22 -0700

    finished homework

--- Java_Day_0/BelgishFlag.java
+++ Java_Day_0/BelgishFlag.java
@@ -3,25 +3,25 @@ import acm.program.*;
 import acm.graphics.*;
 import java.awt.Color;
 
-public class ItalianFlag extends GraphicsProgram
+public class BelgishFlag extends GraphicsProgram
 {
 
     public void run()
     {
-        GRect firstThird = new GRect(20, 20, 40, 60);
-        firstThird.setFilled(true);
-        firstThird.setColor(new Color(0,0,0));
-        add(firstThird);
+        GRect greenThird = new GRect(20, 20, 40, 60);
+        greenThird.setFilled(true);
+        greenThird.setColor(new Color(0,0,0));
+        add(greenThird);
 
         GRect middleThird = new GRect(60, 20, 40, 60);
         middleThird.setFilled(true);
         middleThird.setColor(Color.yellow);
         add(middleThird);
   
-        GRect lastThird = new GRect(100, 20, 40, 60);
-        lastThird.setFilled(true);
-        lastThird.setColor(Color.red);
-        add(lastThird);
+        GRect redThird = new GRect(100, 20, 40, 60);
+        redThird.setFilled(true);
+        redThird.setColor(Color.red);
+        add(redThird);
         
         // this draws a nice black border around the flag
         // notice how I did NOT make it filled, and it is black by default
