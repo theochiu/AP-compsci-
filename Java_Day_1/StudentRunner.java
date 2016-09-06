76755f8b3c5e66711629fb402a4c0f28529030f8
Theodore Chiu <Theodore Chiu>
2016-09-05 20:24:14 -0700

    finished homework

--- Java_Day_1/StudentRunner.java
+++ Java_Day_1/StudentRunner.java
@@ -1,14 +1,24 @@
-
+/* Copyright 2016, Theodore Chiu, All rights reserved. */
 import acm.program.*;
 
 public class StudentRunner extends ConsoleProgram
 {
 
-    public void run()
-    {
-        Student stud = new Student("Theo");
-        stud.takeQuiz(8);
-    }
+	public void run()
+	{
+		Student stud = new Student("Theo");
+		stud.takeQuiz(8);
+
+		String message="";
+		
+		message += stud.getName();
+		message += " got a ";
+		stud.takeQuiz(10);
+		stud.takeQuiz(30);
+		message+=(stud.quizAverage());
+		message+=" in Kmo's class!";
+		println(message);
+	}
 
 
 }
