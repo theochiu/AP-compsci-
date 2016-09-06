4a06f320246fa2863b82e4995db585c431615efa
Theodore Chiu <tchiu17@shs.shschools.org>
2016-09-06 08:34:36 -0700

    ...

--- Java_Day_2/Stoplight.java
+++ Java_Day_2/Stoplight.java
@@ -6,14 +6,44 @@ import java.awt.event.*;
 
 public class Stoplight extends GraphicsProgram
 {
-    private static final double BULB_RADIUS = 60; 
-    private static final double WINDOW_WIDTH = 754; 
-    private static final double WINDOW_HEIGHT = 492;
+	private static final double BULB_RADIUS = 60; 
+	private static final double WINDOW_WIDTH = 754; 
+	private static final double WINDOW_HEIGHT = 492;
 
-    public void run()
-    {
-        
-    }
+	public void run()
+	{
+		GRect box = new GRect(WINDOW_WIDTH/2 - BULB_RADIUS , 
+							  WINDOW_HEIGHT/2-3*BULB_RADIUS , 
+							  2*BULB_RADIUS, 6*BULB_RADIUS);
+		box.setFilled(true);
+		box.setColor(Color.gray);
+		add(box);
+
+		// yellow light
+		GOval yellowLight = new GOval(WINDOW_WIDTH/2 - BULB_RADIUS , 
+									  WINDOW_HEIGHT/2-BULB_RADIUS, 
+									  2*BULB_RADIUS, 2*BULB_RADIUS);
+		yellowLight.setFilled(true);
+		yellowLight.setColor(Color.yellow);
+		add(yellowLight);
+
+		// red light
+		GOval redLight = new GOval(WINDOW_WIDTH/2 - BULB_RADIUS , 
+									  WINDOW_HEIGHT/2-3*BULB_RADIUS, 
+									  2*BULB_RADIUS, 2*BULB_RADIUS);
+		redLight.setFilled(true);
+		redLight.setColor(Color.red);
+		add(redLight);
+
+		// green light 
+
+		GOval greenLight = new GOval(WINDOW_WIDTH/2 - BULB_RADIUS , 
+									  WINDOW_HEIGHT/2+BULB_RADIUS, 
+									  2*BULB_RADIUS, 2*BULB_RADIUS);
+		greenLight.setFilled(true);
+		greenLight.setColor(Color.green);
+		add(greenLight);
+	}
 
 
 }
--- Java_Day_2/ThermostatRunner.java
+++ Java_Day_2/ThermostatRunner.java
@@ -6,7 +6,13 @@ public class ThermostatRunner extends ConsoleProgram
 
     public void run()
     {
-        
+        Thermostat therm = new Thermostat(60,90);
+        therm.warmer();
+        println(therm.getValue());
+        for (int i = 0; i<10;i++){
+            therm.colder();
+        }
+        println(therm.getValue());
     }
 
 }
