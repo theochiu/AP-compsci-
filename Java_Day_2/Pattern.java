6275fc3ddc1699e732a7421e8ee541c945e41eb7
Theodore Chiu <tchiu17@shs.shschools.org>
2016-09-06 08:39:45 -0700

    ...

---
+++ Java_Day_2/Pattern.java
@@ -0,0 +1,16 @@
+
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.Color;
+import java.awt.event.*;
+
+public class Pattern extends GraphicsProgram
+{
+
+    public void run()
+    {
+
+    }
+
+
+}
