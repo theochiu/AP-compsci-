670ba886f18dcdf3346464ff80f2b0bd6fe46c6b
Theodore Chiu <tchiu17@shs.shschools.org>
2016-09-06 08:17:23 -0700

    new files

---
+++ Java_Day_2/ItalianFlag.java
@@ -0,0 +1,12 @@
+
+
+public class ItalianFlag
+{
+
+    public ItalianFlag()
+    {
+
+    }
+
+
+}
---
+++ Java_Day_2/ThermostatRunner.java
@@ -0,0 +1,12 @@
+
+import acm.program.*;
+
+public class ThermostatRunner extends ConsoleProgram
+{
+
+    public void run()
+    {
+        
+    }
+
+}
