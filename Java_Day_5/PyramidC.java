0bacadcbda0031ba88559fa159d6997b333c218f
Theodore Chiu <tchiu17@shs.shschools.org>
2016-09-12 09:23:53 -0700

    ...

--- Java_Day_5/PyramidC.java
+++ Java_Day_5/PyramidC.java
@@ -6,16 +6,25 @@ import java.awt.event.*;
 
 public class PyramidC extends GraphicsProgram
 {
-    private static int BRICK_HEIGHT = 12;
-    private static int BRICK_WIDTH = 30;
-    private static int GAP = 4;
-    private static int NUM_ROWS = 10;
-    private static int START_X = 50;
-    private static int START_Y = 60;
+	private static int BRICK_HEIGHT = 12;
+	private static int BRICK_WIDTH = 30;
+	private static int GAP = 4;
+	private static int NUM_ROWS = 10;
+	private static int START_X = 50;
+	private static int START_Y = 60;
 
-    public void run()
-    {
-       
-    }
+	public void run(){
 
+		for (int r=0; r<NUM_ROWS; r++){
+
+			int missingBricks = NUM_ROWS-r;
+			int shift = missingBricks * (BRICK_WIDTH+GAP)/2;
+
+			for (int c=0; c<r; c++){
+				GRect brick = new GRect(shift+START_X + c*(BRICK_WIDTH+GAP) , START_Y + r*(BRICK_HEIGHT+GAP) , BRICK_WIDTH,BRICK_HEIGHT);
+				brick.setFilled(true);
+				add(brick);
+			}
+		}
+	}
 }
