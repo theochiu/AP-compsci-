b37c49f8bfc6d30d2dc30a15c478c845f857c73d
theochiu <theochiu.me@gmail.com>
2016-09-21 22:53:35 -0700

    ... (#20)

---
+++ Class_making/Employee.java
@@ -0,0 +1,23 @@
+
+public class Employee{
+
+	public String name;
+	public double salary;
+
+	public Employee(String inputName, double inputSalary){
+		name=inputName;
+		salary = inputSalary;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void getSalary(){
+		return salary;
+	}
+
+	public void raiseSalary(double amount){
+
+	}
+}
