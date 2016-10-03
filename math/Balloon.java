b98097cd3bd793d66b759a3e8796b61e5d4023b4
Theodore Chiu <tchiu17@shs.shschools.org>
2016-10-03 13:15:07 -0700

    ...

---
+++ math/Balloon.java
@@ -0,0 +1,31 @@
+
+
+public class Balloon
+{
+	private static final double MAXVOLUME = 10000;
+	private double radius;
+	private boolean popped;
+
+	public Balloon(){
+		radius = 0;
+		popped = false;
+	}
+
+	public void inflate(double ccAdded){
+
+	}
+
+	public double getRadius(){
+
+	}
+
+	public double getSurfaceArea(){
+
+	}
+
+	public double getVolume(){
+
+	}
+
+
+}
