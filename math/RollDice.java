6bbf9d1a694bac948f6ca4c2a560ae47e3473e96
theochiu <theochiu.me@gmail.com>
2016-10-04 22:04:30 -0700

    finished homework

--- math/RollDice.java
+++ math/RollDice.java
@@ -7,7 +7,7 @@ import java.awt.event.*;
 public class RollDice extends GraphicsProgram
 {
 
-	private int face=1;
+	private int face=2;
 
 	public void run()
 	{
@@ -22,7 +22,36 @@ public class RollDice extends GraphicsProgram
 		}
 
 		if (face==2){
-			
+			GOval circle = new GOval(square.getX()+20, square.getY()+20, 100,100);
+			GOval circle1 = new GOval(square.getX()+20+square.getWidth()/2, square.getY()+20+square.getHeight()/2,100,100);
+			circle.setFilled(true);
+			add(circle);
+			circle1.setFilled(true);
+			add(circle1);
+		}
+
+		if (face==3){
+			GOval circle = new GOval(square.getX()+square.getWidth()/2-50, square.getY()+square.getHeight()/2-50,100,100);
+			circle.setFilled(true);
+			add(circle);
+		}
+
+		if (face==4){
+			GOval circle = new GOval(square.getX()+square.getWidth()/2-50, square.getY()+square.getHeight()/2-50,100,100);
+			circle.setFilled(true);
+			add(circle);
+		}
+
+		if (face==5){
+			GOval circle = new GOval(square.getX()+square.getWidth()/2-50, square.getY()+square.getHeight()/2-50,100,100);
+			circle.setFilled(true);
+			add(circle);
+		}
+
+		if (face==6){
+			GOval circle = new GOval(square.getX()+square.getWidth()/2-50, square.getY()+square.getHeight()/2-50,100,100);
+			circle.setFilled(true);
+			add(circle);
 		}
 
 	add(square);
