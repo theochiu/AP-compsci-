8836a4ab779c08f3ad6885e013bf529e01477b9d
Theodore Chiu <tchiu17@shs.shschools.org>
2016-10-20 12:09:41 -0700

    ...

--- Unit_1_Examples_Booleans/Paycheck.java
+++ Unit_1_Examples_Booleans/Paycheck.java
@@ -2,18 +2,23 @@
 
 public class Paycheck
 {
-    private double hoursWorked;
-    private double hourlyRate;
+	private double hoursWorked;
+	private double hourlyRate;
 
-    public Paycheck(double hours, double rate)
-    {
-        hoursWorked = hours;
-        hourlyRate = rate;
-    }
+	public Paycheck(double hours, double rate)
+	{
+		hoursWorked = hours;
+		hourlyRate = rate;
+	}
 
-    public double getAmount()
-    {
-                  
-    }
+	public double getAmount()
+	{
+		if (hoursWorked<=40)
+			double basepay = hoursWorked*hourlyRate;
+		else 
+			double basepay = (hoursWorked -40) * hourlyRate *1.5 + 40*hourlyRate;
+
+		basepay
+	}
 
 }
