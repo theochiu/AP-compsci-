73d7b12bc4496ef4cc8c8da9d1aa014e2189e06c
Theodore Chiu <tchiu17@shs.shschools.org>
2016-10-21 13:58:51 -0700

    ...

--- Unit_1_Examples_Booleans/Paycheck.java
+++ Unit_1_Examples_Booleans/Paycheck.java
@@ -13,12 +13,29 @@ public class Paycheck
 
 	public double getAmount()
 	{
+	    double basepay;
 		if (hoursWorked<=40)
-			double basepay = hoursWorked*hourlyRate;
+			 basepay = hoursWorked*hourlyRate;
 		else 
-			double basepay = (hoursWorked -40) * hourlyRate *1.5 + 40*hourlyRate;
+			 basepay = (hoursWorked -40) * hourlyRate *1.5 + 40*hourlyRate;
 
-		basepay
+		double netpay = 0;
+
+		if (basepay<200)
+			netpay = basepay*(1-.11);
+		else if ( basepay <400)
+			netpay = basepay*(1-.15);
+		else if (basepay<600)
+			netpay = basepay*(1-.18);
+		else 
+			netpay = basepay*(1-.24);
+
+		netpay -=14;
+
+		if (netpay<=0)
+			return 0;
+		else 
+			return netpay;
 	}
 
 }
