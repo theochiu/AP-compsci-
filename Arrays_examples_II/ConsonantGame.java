2fef7054c88336778fa790f6eb86ff26aa3dbc9a
Theodore Chiu <tchiu17@shs.shschools.org>
2016-11-03 10:00:46 -0700

    ...

--- Arrays_examples_II/ConsonantGame.java
+++ Arrays_examples_II/ConsonantGame.java
@@ -7,43 +7,51 @@ import java.util.*;
 
 public class ConsonantGame extends GraphicsProgram
 {
-    private ArrayList<GLabel> labels;
-    private RandomWordGenerator generator;
-    private int turns;
+		private ArrayList<GLabel> labels;
+		private RandomWordGenerator generator;
+		private int turns;
 
-    public void run()
-    {
-        labels = new ArrayList<GLabel>();
-        generator = new RandomWordGenerator();
-        turns = 0;
-        // fill up labels with 25 random labels here
-        addKeyListeners();
-        // play the game and tell me how many turns it took
-        // to win here
-        
-    }
+		public void run()
+		{
+				labels = new ArrayList<GLabel>();
+				generator = new RandomWordGenerator();
+				turns = 0;
+				// fill up labels with 25 random labels here
+				addKeyListeners();
+				// play the game and tell me how many turns it took
+				// to win here
+				
+		}
 
-    public void keyPressed(KeyEvent event)
-    {
-        String letter = KeyEvent.getKeyText(event.getKeyCode()).toUpperCase();  
-        // handle one key press here
-    }
+		public void keyPressed(KeyEvent event)
+		{
+				String letter = KeyEvent.getKeyText(event.getKeyCode()).toUpperCase();  
+				// handle one key press here
+		}
 
-    public GLabel getRandomLabel()
-    {
-        /* Return a GLabel, with a random word, random
-           location, random color, and random font size.
-           It should be added 
-        */
-       
-       String word=generator.getRandomWord();
-       GLabel label = new GLabel(word, (int)( Math.random()*getWidth()),
-                                (int) (Math.random()*getHeight()));
-      Color randomColor = new Color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
-      label.setColor(randomColor);
-      int size= (int) (Math.random()*36)+24;
-      label.setFont("*-*-"+size);
-      return label;
-    }
+		public GLabel getRandomLabel()
+		{
+				/* Return a GLabel, with a random word, random
+					 location, random color, and random font size.
+					 It should be added 
+				*/
+			 
+			 String word=generator.getRandomWord();
+			 GLabel label = new GLabel(word, (int)( Math.random()*getWidth()),
+																			(int) (Math.random()*getHeight()));
+			Color randomColor = new Color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+			label.setColor(randomColor);
+			int size= (int) (Math.random()*36)+24;
+			label.setFont("*-*-"+size);
+
+			while (label.getWidth()+label.getX()>getWidth()){
+				label.setLocation((int)(Math.random()*getWidth()),label.getY());
+			}
+
+
+
+
+			return label;
+		}
 
 }
