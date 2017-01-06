3d9dc83a278c38e65692d6002deefab46851d4f3
Theodore Chiu <tchiu17@shs.shschools.org>
2017-01-06 08:55:39 -0800

    ...

---
+++ ConnectFour2017/ConnectFourGame.java
@@ -0,0 +1,56 @@
+import java.awt.Color;
+import info.gridworld.grid.*;
+
+public class ConnectFourGame
+{
+	private int[][] board;
+	private int currentTurnNumber;
+	
+	public ConnectFourGame(int rows, int cols)
+	{
+		board = new int[rows][cols];
+		// you need to fill board with 0's
+		currentTurnNumber = 1;
+	}
+
+	// returns 0, 1, or 2
+	public int getWinner()
+	{      
+		// complete this in version 1.0
+		
+		return 0; // feel free to eventually change this line.  it is just here so the rest will compile
+	}
+
+	// updates the state of the game (board and currentTurnColor)
+	// returns the row in which the checker would end up
+	// returns -1 if the column col is completely full and no checker can be dropped
+	public int dropChecker(int col)
+	{
+		for (int r = board.length-1; r>=0, r--){
+			if (board[r][col]==0){
+				board[r][col] = currentTurnNumber;
+				if (currentTurnNumber==1){
+					currentTurnNumber = -1;
+				}
+				else 
+					currentTurnNumber=2;
+
+				return r;
+			}
+		}
+		return -1;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ConnectFour2017/ConnectFourGraphicsProgram.java
@@ -0,0 +1,59 @@
+
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.Color;
+import java.awt.event.*;
+
+public class ConnectFourGraphicsProgram extends GraphicsProgram
+{
+    private static final int PIECE_DIAM = 100;
+    private static final int NUM_COLS = 7;
+    private static final int NUM_ROWS = 6;
+    public static final int APPLICATION_WIDTH = NUM_COLS * PIECE_DIAM;
+    public static final int APPLICATION_HEIGHT = NUM_ROWS * PIECE_DIAM;
+
+    /** Dimensions of game board in pixels (usually the same) */
+    private static final int WIDTH = APPLICATION_WIDTH;
+    private static final int HEIGHT = APPLICATION_HEIGHT;
+
+
+    private ConnectFourGame game;
+    private boolean gameIsBeingPlayed;
+    private Color currentDrawingColor;
+
+    
+    public void run()
+    {
+        currentDrawingColor = Color.RED;
+        
+        game = new ConnectFourGame(NUM_ROWS, NUM_COLS);
+        gameIsBeingPlayed = true;
+        addMouseListeners();
+        while (game.getWinner()==0)
+        {
+        }
+        gameIsBeingPlayed = false;
+        
+        // add code here in version 0.4
+    }
+
+    public void mouseClicked(MouseEvent e) 
+    { 
+        if (!gameIsBeingPlayed) return;
+
+        int x = e.getX();
+        int col = x/PIECE_DIAM;
+        int row = game.dropChecker(col);
+        if (row!=-1){
+            drawChecker(row, col)
+        }
+    
+    }
+    
+    public void drawChecker(int rol, int col)
+    {
+        // complete this in version 0.3
+    }
+
+
+}
