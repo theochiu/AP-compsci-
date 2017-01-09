d37df1d6ded570414986b073b3fd7822bd847c27
Theodore Chiu <tchiu17@shs.shschools.org>
2017-01-09 11:34:38 -0800

    sliding puzzle

---
+++ SlidingPuzzle2017/__SHELL5.java
@@ -0,0 +1,10 @@
+
+public class __SHELL5 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\tchiu17\\Downloads\\GitPortable\\Data\\home\\AP-compsci\\SlidingPuzzle2017");
+final SlidingPuzzleGraphics slidingP1 = (SlidingPuzzleGraphics)__bluej_runtime_scope.get("slidingP1");
+
+
+slidingP1.start();
+
+}}
